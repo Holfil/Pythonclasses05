8066faec7982dbc42646458f584d6e85b7eb1740
Xander-Way <abholfil@gmail.com>
2026-04-14 22:22:23 +0400

    Assignment Two

---
+++ Assignment/assignment_2.py
@@ -0,0 +1,29 @@
+# ALEXANDER BOTCHWAY_ASSIGNMENT 2
+
+
+# Challenge 1: Mobile Money Deposit Checker
+
+# amount = float(input("Enter the amount deposited: "))
+# if amount > 1000:
+#    print("This is a large deposit. Verify customer details.")
+
+
+# Challenge 2: Student Exam Result Checker
+# score = float(input("Enter the student's score: "))
+# if score >= 50:
+#     print("You passed the exam")
+
+# Challenge 3: ATM Withdrawal Alert
+# amount = float(input("Enter the withdrawal amount: "))
+# if amount > 50:
+#     print("Alert: Large withdrawal detected.")
+
+# Challenge 4: Age-Based Voting Eligibility (Basic Version)
+# age = float(input("Enter  aage: "))
+# if age >= 18:
+#     print("You are Eligible to vote.")
+
+# Challenge 5: Shopping Free Delivery 
+# cost = float(input("Enter total cost: "))
+# if cost > 200:
+#     print("You qualify for free delivery.")
