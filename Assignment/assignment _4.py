d6a0bf2394972629c561612c93183cdb855d94d9
Xander-Way <abholfil@gmail.com>
2026-04-20 02:42:17 +0400

    Assigment 4

---
+++ Assignment/assignment _4.py
@@ -0,0 +1,44 @@
+#            ALEXANDER_BOTCHWAY:
+
+
+# Challenges 1: Shop Discount System
+
+# price = float(input("Enter the price: "))
+# discount = 0.1
+# if price > 100:
+# discounted_price = price * (1 - discount)
+#     print (f"{discounted_price}")
+# else:    
+#   print (f"{price}")
+
+
+# Challenge 2: Even or Odd Checker
+# for number in range(5):
+#    number = int(input("Enter a number: "))
+#    if number % 2 == 0:
+#        print(f"{number} is even.")
+#    else: 
+#        print(f"{number} is odd.")
+
+# Challenge 3: Countdown Timer
+ 
+# number = int(input("Enter a number to count down from: "))
+# while number >= 1:
+#    print(number)
+#    number -= 1  
+
+# Challenge 4: Name Repeater
+
+# name = input("Enter your name: ")
+# number = int(input("Enter a number: "))
+# for i in range(number):
+#     print(name)
+
+# Challenge 5: Number Guessing Game
+# secret_number = 7
+# guess = int(input("Guess the secret number: "))
+# while guess == secret_number:
+#    print("Correct!")
+#    break
+# else:    
+#    print("Try again.")
